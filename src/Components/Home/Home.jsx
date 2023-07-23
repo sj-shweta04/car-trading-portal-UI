29846fe615de719d2064a03b2debeb36e3c481c3
shweta_jadhav <sj.shweta@gmail.com>
2023-07-23 21:59:23 +0530

    Removed text

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,7 +4,6 @@ const Home = () => {
   return (
     <>  
     <div className="bg-image container vh-100 d-flex">
-    <h1>Home</h1>
     <div className='d-flex align-items-center justify-content-center'>
     <Search/>
     </div>
