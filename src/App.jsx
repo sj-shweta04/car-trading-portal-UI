f27aac680a7fb3c82d516d233561368aa87fb16e
shweta_jadhav <sj.shweta@gmail.com>
2023-07-29 15:11:33 +0530

    Navbar design added and onChange added to search

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import Home from './Components/Home/Home'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
 
   return (
     <>
+  <Navbar/>
   <Home/>
     </>
   )
--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { useState } from 'react';
 import './Style.css'
 import { Box, Button, Autocomplete, TextField } from '@mui/material';
-const Search = () => {
+export const Search =(props)=> (
+  <Autocomplete
+  disablePortal
+  id={props.id}
+  options={props.options}
+  onChange={props.onChange}
+  sx={{...props.style}}
+  renderInput={(params) => <TextField {...params} label={props.label}/>}
+/>
+)
+
+const style = { width: 200, pt: 4, pl: 2 }
+
+
+const SearchPanel = () => {
+  const [selected , setSelectedValue] = useState({searchByCityHome:'',searchByBrand:''})
+  
+  const handleAutocompleteChange = (event, newValue) => {
+    const id = event.target.id.split('-')[0]
+   setSelectedValue({...selected,[id]:newValue});
+  };
+
+  const doSearch = ()=>{
+    console.log('search',selected);
+  }
   return (
     <Box component="span" sx={{
       width: 300,
@@ -15,25 +39,13 @@ const Search = () => {
 
     }}>
       <h3 className='head-title'>Find Your Dream Car</h3>
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={['car1', 'car2']}
-        sx={{ width: 200, pt: 4, pl: 2 }}
-        renderInput={(params) => <TextField {...params} label="Search By City" />}
-      />
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={['brand1', 'brand2']}
-        sx={{ width: 200, pt: 4, pl: 2 }}
-        renderInput={(params) => <TextField {...params} label="Search By Brand" />}
-      />
+      <Search id={'searchByCityHome'} label={'Search By City'} options={['car1','car2','car3']} style={style} onChange={handleAutocompleteChange}/>
+      <Search id={'searchByBrand'} label={'Search By Brand'} options={['city1','city2','city3']} style={style} onChange={handleAutocompleteChange}/>
       <div className='align-btn'>
-        <Button variant="contained" sx={{ color: 'text.primary' }}>Search</Button>
+        <Button variant="contained" onClick={doSearch} sx={{ color: 'text.primary' }}>Search</Button>
       </div>
     </Box>
   )
 }
 
-export default Search
+export default SearchPanel
