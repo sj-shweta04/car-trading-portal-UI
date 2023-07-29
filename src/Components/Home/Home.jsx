ac907432fcef6c135697f79f4aadec92181e18bd
shweta_jadhav <139198850+sj-shweta04@users.noreply.github.com>
2023-07-29 15:19:07 +0530

    Navbar design added and onChange added to search (#2)
    Co-authored-by: shweta_jadhav <sj.shweta@gmail.com>

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,11 @@
-import Search from '../Search/Search'
+import SearchPanel from '../Search/Search'
 import './style.css'
 const Home = () => {
   return (
     <>  
     <div className="bg-image container vh-100 d-flex">
     <div className='d-flex align-items-center justify-content-center'>
-    <Search/>
+    <SearchPanel/>
     </div>
     </div>
     </>
