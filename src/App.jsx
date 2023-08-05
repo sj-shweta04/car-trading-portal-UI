fcfd24b134ed0e418b0a556b7851ad827edd43e3
shweta_jadhav <sj.shweta@gmail.com>
2023-08-05 22:10:29 +0530

    Login/signUp ui and validation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-
+import { useState } from 'react';
+import { LoginContext } from './Components/Context/LoginContext';
 import Navbar from './Components/Navbar/Navbar'
 import { Outlet } from "react-router-dom";
 
 function App() {
 
-
+  const [login, setLogin] = useState(false)
   return (
     <>
-      <Navbar />
-      <Outlet/>
+      <LoginContext.Provider value={{ login, setLogin }}>
+        <Navbar />
+        <Outlet />
+      </LoginContext.Provider>
     </>
   )
 }
