9638ac1925683449f4472e3309e834bb01b851e9
sj-shweta04 <sj.shweta014@gmail.com>
2023-09-04 00:04:12 +0530

    aboutUs

--- src/Components/AboutPage/AboutUs.jsx
+++ src/Components/AboutPage/AboutUs.jsx
@@ -1,10 +1,27 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
-const AboutUs = () => {
+export default function AboutUs() {
   return (
-    <div>
-      AboutUs
-    </div>
-  )
-}
+    <React.Fragment>
+      {/* <CssBaseline /> */}
+      <Container maxWidth="xl" sx={{paddingTop:'30px'}}>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '50vh', width:'100vh'}}>
+         
+        <div style={{paddingTop:'10px',paddingLeft:'10px', paddingRight:'50px'}}>
+    <b><i>About Trade-Cars</i></b> <br/>
+Trade-Cars is India's leading car search venture that helps users buy second-hand cars that are right for them. Its website and app carry rich automotive content such as detailed specs and prices, pictures of all car brands and models available in India. <br/> 
+Trade-Cars has launched many innovative features to ensure that users get an immersive experience of the car model before visiting a dealer showroom. The platform also has used car classifieds wherein users can upload their cars for sale, and find used cars for buying from individuals and used car dealers. <br/>
 
-export default AboutUs
+Our vision is to construct a complete ecosystem for consumers and car manufacturers, dealers and related businesses such that consumers have easy and complete access to buying and selling cars.
+    </div>
+
+        </Box>
+
+        
+      </Container>
+    </React.Fragment>
+  );
+}
